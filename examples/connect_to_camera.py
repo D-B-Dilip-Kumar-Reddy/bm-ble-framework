b97be3c2336277e8e234419b58679000d2113118
DILIP KUMAR REDDY DANDU BHASKAR (CW) <dilip.kumar.reddy.dandu.bhaskar@sandisk.com>
2026-06-09 15:35:13 +0530

    Add connect to camera example

--- examples/connect_to_camera.py
+++ examples/connect_to_camera.py
@@ -1,8 +1,9 @@
 import asyncio
 import logging
 
+from bmd_ble import CameraProfile
+from bmd_ble.camera_controller import BMDCameraController
 from bmd_ble.scanner import scan_for_camera
-from bmd_ble.camera_profile import CameraProfile
 
 MODEL_KEY = "POCKET_6K_G2"
 FIRMWARE = "v7.9"
@@ -11,6 +12,9 @@ async def main():
     cam_profile = CameraProfile.for_model(model_key=MODEL_KEY, firmware=FIRMWARE)
     discovered_camera = await scan_for_camera(cam_profile.ble_name)
     print(discovered_camera)
+    cam = BMDCameraController(discovered_camera)
+    await cam.connect()
+    print("Connected to {}".format(cam.discovered.ble_name))
 
 if __name__ == "__main__":
     logging.basicConfig(
